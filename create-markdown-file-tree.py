310ee831c8ee94effb021d3146b872d93fcddcd9
Rodrigo Martín Núñez <83213317+P-ict0@users.noreply.github.com>
2024-01-22 11:24:25 +0100

    Add arguments

--- create-markdown-file-tree.py
+++ create-markdown-file-tree.py
@@ -1,49 +1,50 @@
 import sys
 import os
 import urllib.parse
+import argparse
 
-def main(dirname: str) -> None:
-    tree = generate_tree(dirname)
-    print("\n".join(tree))
+class tocGenerator():
+    def __init__(self, dirname: str, ignore_hidden: bool = False, url_encode: bool = False) -> None:
+        self.dirname = dirname
+        self.ignore_hidden = ignore_hidden
+        self.url_encode = url_encode
 
-def read_dir(dirname: str) -> list[str]:
-    files = []
-    for root, dirs, filenames in os.walk(dirname):
-        for file in filenames:
-            file_path = os.path.relpath(os.path.join(root, file), dirname)
-            files.append(file_path)
-    return files
+    def generate(self) -> None:
+        tree = self.create_tree()
+        print("\n".join(tree))
 
-def print_tree(tree: list[str]) -> str:
-    return '\n'.join(tree)
+    def create_tree(self) -> list[str]:
+        contents = []
+        for root, dirs, filenames in os.walk(self.dirname):
+            if self.ignore_hidden:
+                # Ignore hidden directories
+                dirs[:] = [d for d in dirs if not d.startswith('.')]
+                # Ignore hidden files
+                filenames[:] = [f for f in filenames if not f.startswith('.')]
 
-def generate_tree(dirname: str) -> list[str]:
-    contents = []
-    for root, dirs, filenames in os.walk(dirname):
-        # Ignore hidden directories
-        dirs[:] = [d for d in dirs if not d.startswith('.')]
-        # Ignore hidden files
-        filenames[:] = [f for f in filenames if not f.startswith('.')]
+            level = root.replace(self.dirname, '').count(os.sep)
+            subindent = '  ' * (level)
+            for file in filenames:
+                file_path = os.path.relpath(os.path.join(root, file), self.dirname).replace("\\","/")
 
-        level = root.replace(dirname, '').count(os.sep)
-        subindent = '  ' * (level)
-        for file in filenames:
-            file_path = os.path.relpath(os.path.join(root, file), dirname).replace("\\","/")
+                # Url encode the file path
+                if self.url_encode:
+                    file_path = urllib.parse.quote(file_path)
 
-            # Url encode the file path
-            processed_file_path = urllib.parse.quote(file_path)
+                # Remove file extension from link text
+                link_text = os.path.splitext(file)[0]
+                contents.append(f'{subindent}- [{link_text}]({file_path})')
 
-            # Remove file extension from link text
-            link_text = os.path.splitext(file)[0]
-            contents.append(f'{subindent}- [{link_text}]({processed_file_path})')
-
-    return contents
+        return contents
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Usage: python3 create-markdown-file-tree.py <dirname>')
-        sys.exit(1)
+    
+    parser = argparse.ArgumentParser(description='Generate Contents with links to files of a given directory')
+    parser.add_argument('--ignore-hidden', '--ih', action='store_true', help='Ignore hidden files and directories')
+    parser.add_argument('--url-encode', '--ue', action='store_true', help='Don`t url encode the file path')
+    parser.add_argument('dirname', type=str, help='Directory to generate contents for')
+    args = parser.parse_args()
 
-    dirname = sys.argv[1]
-    main(dirname)
+    toc = tocGenerator(args.dirname, args.ignore_hidden, args.url_encode)
+    toc.generate()
